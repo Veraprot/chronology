0f43c110d0c1af153c7a1b10b5e8a604ea200c03
Veraprot <vera.protopopova@gmail.com>
2018-11-07 17:44:59 -0500

    filter api results

--- app/models/Request.rb
+++ app/models/Request.rb
@@ -5,7 +5,7 @@ require 'active_support/core_ext/hash'
 
 class Request
   def make_request
-    base_url = 'http://www.vizgr.org/historical-events/search.php?begin_date=17000101&end_date=17201231'
+    base_url = 'http://www.vizgr.org/historical-events/search.php?begin_date=11500101&end_date=12501231'
     s = Net::HTTP.get_response(URI.parse(base_url)).body
     end_input = Hash.from_xml(s)
     end_input 
@@ -13,6 +13,10 @@ class Request
 
   def process_request
     result_hash = make_request
-    result_hash.class
+    filtered_hash = {}
+    result_hash["result"]["event"].each do |event|
+      filtered_hash[event["date"]] = event["description"]
+    end 
+    filtered_hash
   end 
 end 
