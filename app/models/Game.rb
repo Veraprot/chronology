56b2e07c65e918c2fc6a9c0b48538f363afa5617
Veraprot <vera.protopopova@gmail.com>
2018-11-08 16:30:01 -0500

    add cards table

--- app/models/Game.rb
+++ app/models/Game.rb
@@ -2,4 +2,5 @@ class Game < ActiveRecord::Base
   has_many :players
   has_many :participants 
   has_many :players, through: :participants
+  has_many :cards
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,12 @@
-ActiveRecord::Schema.define(version: 2018_11_06_182139) do
+ActiveRecord::Schema.define(version: 2018_11_08_212310) do
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "date"
+    t.text "event"
+    t.integer "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.integer "number_of_players"
